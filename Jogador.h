5141a0dd455cda3ee05009decd60e21ede323726
YagoConstantino <yagor1beiro1603@gmail.com>
2024-11-28 13:55:00 -0300

    Salvando

--- Jogador.h
+++ Jogador.h
@@ -7,7 +7,7 @@ namespace Entidades
 	{
 	private:
 		int _pontos;
-		float _speed;
+		float _speed; // Sera que pode ser um sf::vector2f ?
 	public:
 		Jogador(float inlX = 0, float inY = 0,
 			float H = 0, float W = 0, Gerenciadores::Gerenciador_Grafico* pgra = nullptr);
