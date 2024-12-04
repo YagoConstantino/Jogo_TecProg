a4885bcb08c3071242061dca9ae5129217c3c99e
YagoConstantino <yagor1beiro1603@gmail.com>
2024-12-04 12:09:42 -0300

    Atributo onGround booleano em Personagem

--- Personagem.cpp
+++ Personagem.cpp
@@ -1,10 +1,20 @@
 #include "Personagem.h"
 
 Entidades::Personagem::Personagem(float inicialX, float inicialY, Gerenciadores::Gerenciador_Grafico* pgra )
-	:Entidade(inicialX,inicialY,pgra),_num_vidas(4)
+	:Entidade(inicialX,inicialY,pgra),_num_vidas(4),onGround(false)
 {
 }
 
 Entidades::Personagem::~Personagem()
 {
+}
+
+bool Entidades::Personagem::getOnGround() const
+{
+	return onGround;
+}
+
+void Entidades::Personagem::setBool(bool ong)
+{
+	onGround = ong;
 }
--- Personagem.h
+++ Personagem.h
@@ -7,10 +7,14 @@ namespace Entidades
 	{
 	protected:
 		int _num_vidas;
+		bool onGround;
 
 	public:
 		Personagem(float inicialX = 0, float inicialY = 0, Gerenciadores::Gerenciador_Grafico* pgra = nullptr);
 		virtual ~Personagem();
+
+		bool getOnGround()const;
+		void setBool(bool ong);
 
 		virtual void executar() = 0;
 		virtual void salvar() = 0;
